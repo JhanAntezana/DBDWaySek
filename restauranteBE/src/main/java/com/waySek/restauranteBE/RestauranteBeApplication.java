15d73d2e821dc60ba64b26ea362a61f25527cafd
JhanAntezana <jhan.antezana.y@uni.pe>
2022-12-02 11:14:51 -0500

    first commit

---
+++ restauranteBE/src/main/java/com/waySek/restauranteBE/RestauranteBeApplication.java
@@ -0,0 +1,13 @@
+package com.waySek.restauranteBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteBeApplication.class, args);
+	}
+
+}
---
+++ restauranteBE/src/test/java/com/waySek/restauranteBE/RestauranteBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waySek.restauranteBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
